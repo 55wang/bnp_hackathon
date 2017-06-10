815ad58727cdad2a9c3526d66b2afb2e8ad072c5
Wang Zhe <wangzhe.lynx@gmail.com>
2017-06-10 15:50:11 +0800

    added ticker name

--- crawldata.py
+++ crawldata.py
@@ -5,53 +5,85 @@ import pandas_datareader as pdr
 
 quandl.ApiConfig.api_key = "wh4e1aGKQwZyE4RXWP7s"
 
-INTC = quandl.get("EOD/INTC")
-INTC.reset_index(level=0, inplace=True)
-INTC['bid'] = '35.72 x 1000'
-INTC['ask'] = '36.05 x 600'
-INTC.to_csv('intc.csv', index=False)
-print INTC.head()
+# intel
+# INTC = quandl.get("EOD/INTC")
+# INTC.reset_index(level=0, inplace=True)
+# INTC['bid'] = '35.72 x 1000'
+# INTC['ask'] = '36.05 x 600'
+# INTC.to_csv('intc.csv', index=False)
+# print INTC.head()
 
-jpy = quandl.get("CUR/JPY")
-jpy.reset_index(level=0, inplace=True)
-jpy['bid'] = 0.009
-jpy['ask'] = 0.009
-jpy.to_csv('jpy.csv', index=False)
-print jpy.head()
+# japan currency
+# jpy = quandl.get("CUR/JPY")
+# jpy.reset_index(level=0, inplace=True)
+# jpy['bid'] = 0.009
+# jpy['ask'] = 0.009
+# jpy.to_csv('jpy.csv', index=False)
+# print jpy.head()
 
-# data = quandl.get("CUR/SGD")
+# MORGAN STANLEY
+# data = quandl.get("EOD/MS")
 # data.reset_index(level=0, inplace=True)
-# data['bid'] = '1.384'
-# data['ask'] = '1.386'
-# data.to_csv('sgd.csv', index=False)
+# data['bid'] = '0.00 x'
+# data['ask'] = '0.00 x'
+# data.to_csv('MS.csv', index=False)
+# print data.head()
+#
+# Wells Fargo Utilities
+# data = quandl.get("EOD/ERH")
+# data.reset_index(level=0, inplace=True)
+# data['bid'] = '0.00 x'
+# data['ask'] = '0.00 x'
+# data.to_csv('ERH.csv', index=False)
+# print data.head()
+#
+# NBCUNIVERSAL MED
+# data = quandl.get("EOD/MED")
+# data.reset_index(level=0, inplace=True)
+# data['bid'] = '0.00 x'
+# data['ask'] = '0.00 x'
+# data.to_csv('MED.csv', index=False)
 # print data.head()
 
-data = quandl.get("EOD/MS")
-data.reset_index(level=0, inplace=True)
-data['bid'] = '0.00 x'
-data['ask'] = '0.00 x'
-data.to_csv('MS.csv', index=False)
-print data.head()
+# MORG.STAN. EMTN
+# data = quandl.get("EOD/BARL")
+# data.reset_index(level=0, inplace=True)
+# data['bid'] = '0.00 x'
+# data['ask'] = '0.00 x'
+# data.to_csv('BARL.csv', index=False)
+# print data.head()
+#
+# GLDM SACHS GRP
+# data = quandl.get("SIX/US38141G1040USD4")
+# data.reset_index(level=0, inplace=True)
+# data['bid'] = '0.00 x'
+# data['ask'] = '0.00 x'
+# data.to_csv('US38141G1040USD4.csv', index=False)
+# print data.head()
 
-data = quandl.get("EOD/ERH")
-data.reset_index(level=0, inplace=True)
-data['bid'] = '0.00 x'
-data['ask'] = '0.00 x'
-data.to_csv('ERH.csv', index=False)
-print data.head()
+# JPMORGAN CHASE
+# data = quandl.get("EOD/JPM_P_C")
+# data.reset_index(level=0, inplace=True)
+# data['bid'] = '0.00 x'
+# data['ask'] = '0.00 x'
+# data.to_csv('JPM_P_C.csv', index=False)
+# print data.head()
 
-data = quandl.get("EOD/MED")
-data.reset_index(level=0, inplace=True)
-data['bid'] = '0.00 x'
-data['ask'] = '0.00 x'
-data.to_csv('MED.csv', index=False)
-print data.head()
-
-
-
-
-
-
+# US8825081040
+# data = quandl.get("SSE/WHA")
+# data.reset_index(level=0, inplace=True)
+# data['bid'] = '0.00 x'
+# data['ask'] = '0.00 x'
+# data.to_csv('WHA.csv', index=False)
+# print data.head()
+#
+# HK0004000045
+# data = quandl.get("SSE/TII")
+# data.reset_index(level=0, inplace=True)
+# data['bid'] = '73.04'
+# data['ask'] = '73.13'
+# data.to_csv('TII.csv', index=False)
+# print data.head()
 
 
 
--- stock.py
+++ stock.py
@@ -3,24 +3,42 @@ import pandas as pd
 import pandas_datareader.data as web
 import matplotlib.pyplot as plt
 import sys
+import scipy.optimize as sco
 
-symbols = ['intc', 'jpy']
+
+symbols = ['intc', 'jpy', 'ms', 'erh']
 noa = len(symbols)
 
 data = pd.DataFrame()
 intc = pd.read_csv('intc.csv')
 intc = intc.ix[(intc['Date'] >= '2010-01-01') & (intc['Date'] <= '2016-12-31'), ['Date', 'Adj_Close']]
+intc.columns = ['intc_date', 'intc_close']
 
 jpy = pd.read_csv('jpy.csv')
 jpy = jpy.ix[(jpy['DATE'] >= '2010-01-01') & (jpy['DATE'] <= '2016-12-31'), ['DATE', 'RATE']]
+jpy.columns = ['jpy_date', 'jpy_rate']
 
-total = pd.merge(intc, jpy, how='inner', left_on='Date', right_on='DATE')
+total = pd.merge(intc, jpy, how='inner', left_on='intc_date', right_on='jpy_date')
 
-data['intc'] = total['Adj_Close']
-data['jpy'] = total['RATE']
+ms = pd.read_csv('MS.csv')
+ms = ms.ix[(ms['Date'] >= '2010-01-01') & (ms['Date'] <= '2016-12-31'), ['Date', 'Adj_Close']]
+ms.columns = ['ms_date', 'ms_close']
+
+total = pd.merge(total, ms, how='inner', left_on='intc_date', right_on='ms_date')
+
+erh = pd.read_csv('ERH.csv')
+erh = erh.ix[(erh['Date'] >= '2010-01-01') & (erh['Date'] <= '2016-12-31'), ['Date', 'Adj_Close']]
+erh.columns = ['erh_date', 'erh_close']
+
+total = pd.merge(total, erh, how='inner', left_on='intc_date', right_on='erh_date')
+
+
+data['intc'] = total['intc_close']
+data['jpy'] = total['jpy_rate']
+data['ms'] = total['ms_close']
+data['erh'] = total['erh_close']
 
 data.columns = symbols
-print data['jpy'].head()
 
 print data.head()
 rets = np.log(data / data.shift(1))
@@ -28,13 +46,13 @@ print rets
 print rets.mean()
 print rets.cov()
 
-weights = np.random.random(noa)
-weights /= np.sum(weights)
-print weights
-
-print np.sum(rets.mean() * weights) * 252
-print np.dot(weights.T, np.dot(rets.cov() * 252, weights))
-print np.sqrt(np.dot(weights.T, np.dot(rets.cov() * 252, weights)))
+# weights = np.random.random(noa)
+# weights /= np.sum(weights)
+# print weights
+#
+# print np.sum(rets.mean() * weights) * 252
+# print np.dot(weights.T, np.dot(rets.cov() * 252, weights))
+# print np.sqrt(np.dot(weights.T, np.dot(rets.cov() * 252, weights)))
 
 prets = []
 pvols = []
@@ -52,13 +70,68 @@ pvols = np.array(pvols)
 print pweights
 print prets
 print pvols
+#
+# plt.figure(figsize=(16, 8))
+# plt.scatter(pvols, prets, c=prets / pvols, marker='o')
+# plt.grid(True)
+# plt.xlabel('expected volatility')
+# plt.ylabel('expected return')
+# plt.colorbar(label='Sharpe ratio')
+# plt.show()
 
-plt.figure(figsize=(8, 4))
-plt.scatter(pvols, prets, c=prets / pvols, marker='o')
+def statistics(weights):
+    ''' Returns portfolio statistics.
+                     Parameters
+                     ==========
+                     weights : array-like
+                         weights for different securities in portfolio
+                     Returns
+                     =======
+                     pret : float
+                         expected portfolio return
+                     pvol : float
+                         expected portfolio volatility
+                     pret / pvol : float
+                         Sharpe ratio for rf=0
+                     '''
+    weights = np.array(weights)
+    pret = np.sum(rets.mean() * weights) * 252
+    pvol = np.sqrt(np.dot(weights.T, np.dot(rets.cov() * 252, weights)))
+    return np.array([pret, pvol, pret / pvol])
+
+cons = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
+bnds = tuple((0, 1) for x in range(noa))
+
+
+def min_func_variance(weights):
+    return statistics(weights)[1] ** 2
+
+optv = sco.minimize(min_func_variance, noa * [1. / noa,],
+                                    method='SLSQP', bounds=bnds, constraints=cons)
+
+print
+print optv
+print
+print 'best optimised weight'
+print optv['x'].round(3)
+print 'optimised returns: ', statistics(optv['x'])[0]
+print 'optimised variance: ', statistics(optv['x'])[1]
+
+plt.figure(figsize=(16, 8))
+plt.scatter(pvols, prets,
+         c=prets / pvols, marker='o')
+         # random portfolio composition
+plt.plot(statistics(optv['x'])[1], statistics(optv['x'])[0],
+      'y*', markersize=15.0)
+         # minimum variance portfolio
+
+
 plt.grid(True)
 plt.xlabel('expected volatility')
 plt.ylabel('expected return')
 plt.colorbar(label='Sharpe ratio')
 plt.show()
 
-print pweights[0][1]
+
+
+
